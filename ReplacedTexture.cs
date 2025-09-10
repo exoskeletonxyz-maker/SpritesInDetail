0b538d5ce82afc5b084b54218d40907e22e5476c
exoskeletonxyz-maker <exoskeletonxyz@gmail.com>
2025-09-11 00:07:03 +0700

    Update ReplacedTexture.cs

--- ReplacedTexture.cs
+++ ReplacedTexture.cs
@@ -1,20 +1,32 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace SpritesInDetail
 {
     internal class ReplacedTexture : Texture2D
     {
-        public Texture2D OriginalTexture {get; set;}
-        public Texture2D? NewTexture { get; set; }
+        public Texture2D OriginalTexture { get; private set; }
+        public Texture2D? NewTexture { get; private set; }
 
-        public HDTextureInfo HDTextureInfo {get; set;}
-        
-        public ReplacedTexture(Texture2D originalTexture, Texture2D? newTexture, HDTextureInfo hdTextureInfo, int? width = null, int? height = null) 
+        public HDTextureInfo HDTextureInfo { get; private set; }
+
+        public ReplacedTexture(Texture2D originalTexture, Texture2D? newTexture, HDTextureInfo hdTextureInfo, int? width = null, int? height = null)
             : base(originalTexture.GraphicsDevice, width ?? originalTexture.Width, height ?? originalTexture.Height)
         {
-            this.OriginalTexture = originalTexture;
-            this.NewTexture = newTexture;
-            this.HDTextureInfo = hdTextureInfo;
+            OriginalTexture = originalTexture;
+            NewTexture = newTexture;
+            HDTextureInfo = hdTextureInfo;
+        }
+
+        // Helper method untuk Android-safe replacement
+        public void ApplyNewTexture()
+        {
+            if (NewTexture != null)
+            {
+                // Copy pixel data untuk Android
+                Color[] data = new Color[NewTexture.Width * NewTexture.Height];
+                NewTexture.GetData(data);
+                this.SetData(data);
+            }
         }
     }
 }
